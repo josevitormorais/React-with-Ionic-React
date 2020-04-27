825836fa95ad224f647af8567a1c1ebdb6c0ee05
jose vitor <josevitormorais7@gmail.com>
2020-04-27 13:48:09 -0300

    Initial commit

---
+++ src/pages/Tabs/Search.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { IonPage, IonHeader, IonToolbar, IonTitle } from "@ionic/react";
+
+const Serach = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Serach</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+    </IonPage>
+  );
+};
+export default Serach;
